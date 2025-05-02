8b0043c14a50ff7f036aa98fc4550da8dd0af70a
Mohammad Mahdi Salmani <mehdismi70@gmail.com>
2025-05-02 14:35:33 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
 nohup python manage.py runserver 0.0.0.0:8000 &
 deactivate
